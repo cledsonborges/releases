3bf1166721cd1c8e62506143a9227641bbacb82b
Your Name <you@example.com>
2025-06-26 16:15:25 -0300

    auto

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: '/dashboard' }
-];
-
